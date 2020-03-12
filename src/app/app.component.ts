397893cae2101b89f3a1f293a2b51f995a64601d
sdyalor <sdyalor@outlook.com>
2020-03-12 09:22:28 -0500

    rx ex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, range } from 'rxjs';
+import { Observable, range, from } from 'rxjs';
 import {map,filter,scan} from 'rxjs/operators';
 
 @Component({
@@ -17,6 +17,13 @@ export class AppComponent implements OnInit{
           filter(x=>x>100),
           pow(3)
         ).subscribe(x=>console.log(x));
+
+        console.log('here');
+
+      const source = from([1,2,3,4,5,6,7,8,9]);   
+      const example = source.pipe(map(val=>val+10));
+      const subscribe = example.subscribe(val=>console.log(val));
+
   }
   title = 'lintering';
 }
