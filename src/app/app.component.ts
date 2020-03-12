c94d4d5716f7dc86cd1d5c353195405ddba0cdc5
sdyalor <sdyalor@outlook.com>
2020-03-12 03:01:55 -0500

    tsconfig ignore tsversion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, range } from 'rxjs';
+import {map,filter,scan} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    const pow = (p: number) =>
+      (source: Observable<number>) =>
+        source.pipe(map(n => n ** p));
+        const source$ = range(0,10);
+        source$.pipe(
+          filter(x=>x>100),
+          pow(3)
+        ).subscribe(x=>console.log(x));
+  }
   title = 'lintering';
 }
